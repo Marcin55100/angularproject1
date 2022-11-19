b14f6e2172ebf3d08fc3a010422ce467482a3849
Marcin Koz³owski <marcin.kozlowski@dgt.eu>
2022-11-19 18:54:21 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularproject1';
+  public forecasts?: WeatherForecast[];
+  isDisabled: boolean = false;
+  title: string = "My title";
+  value = 0;
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+
+  onClick() {
+    console.log("Button has clicked");
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
