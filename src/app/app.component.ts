dbbdbbf786585d2e8c0fe2f8d81a4a9f241476ae
Marcin Koz³owski <marcin.kozlowski@dgt.eu>
2022-12-30 01:14:23 +0100

    add logo, remove unnecessary comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,43 +7,10 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  //@ViewChild(MatTable, { static: false }) table: MatTable<any>;
-  //displayedColumns: string[] = ['home', 'away', 'score'];
-  //public results?: MatchResult[];
-  //public realResults?: MatchResult[];
-  //public dataSource: MatTableDataSource<MatchResult> = new MatTableDataSource<MatchResult>([]);
-  description: string = "This component demonstrates fetching data from livescore...";
+  description: string = "This page demonstrates fetching data from livescore...";
   team: string = "Arsenal";
 
   constructor(public http: HttpClient) {
     console.log("app constructor entered");
   }
-
-  //  http.get<MatchResult[]>('/footballresults/fake').subscribe(result => {
-  //    this.results = result;
-  //    this.dataSource = new MatTableDataSource<MatchResult>(result);
-  //    console.log(result);
-  //  }, error => console.error(error));
-
-  //  http.get<MatchResult[]>('/footballresults').subscribe(result => {
-  //    this.realResults = result;
-  //    this.dataSource = new MatTableDataSource<MatchResult>(result);
-  //    console.log(result);
-  //  }, error => console.error(error));
-  //}
-
-  //getData() {
-  //  this.http.get<MatchResult[]>('/footballresults/fake').subscribe(result => {
-  //    this.dataSource = new MatTableDataSource<MatchResult>(result);
-  //    console.log(result);
-  //  }, error => console.error(error));
-  //}
 }
-
-//export interface MatchResult {
-//  homeTeam: string;
-//  homeScore: number;
-//  awayTeam: string;
-//  awayScore: number;
-//}
